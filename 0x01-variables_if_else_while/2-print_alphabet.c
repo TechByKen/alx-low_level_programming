1c0e9ae36ca8f63f7f5bb196c3c79cc89e708627
Kenetechz <diala.kenneth98@gmail.com>
2023-07-14 16:10:53 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
