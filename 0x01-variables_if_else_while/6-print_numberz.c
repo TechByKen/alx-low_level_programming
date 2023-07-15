edd601c1414c005cbde614839a8a4a1224907008
Kenetechz <diala.kenneth98@gmail.com>
2023-07-14 17:35:30 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+char number;
+
+for (number = '0'; number <= '9'; number++)
+putchar(number);
+putchar('\n');
+return (0);
+}
