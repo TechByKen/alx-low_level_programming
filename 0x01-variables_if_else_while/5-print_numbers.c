26ae1afecb504a915370800aeb0f736402019841
Kenetechz <diala.kenneth98@gmail.com>
2023-07-14 17:15:43 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+
+int number;
+
+for (number = 0; number < 10; number++)
+putchar('0' + number);
+putchar('\n');
+return (0);
+}
