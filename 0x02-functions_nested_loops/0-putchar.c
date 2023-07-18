b015a7d82f26ec037ca7820ec92319d1faa45f1a
Kenetechz <diala.kenneth98@gmail.com>
2023-07-18 08:57:17 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
+
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
+
+void print_alphabet_x10(void) {
+    for (int i = 0; i < 10; i++) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            _putchar(c);
+        }
+    }
+    _putchar('\n');
+}
+
+int main() {
+    print_alphabet_x10();
+    return 0;
+}
 #include "main.h"
 
 /**
