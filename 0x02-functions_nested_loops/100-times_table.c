f10c0120781c3f64c8694eacfe613aad135699a7
Kenetechz <diala.kenneth98@gmail.com>
2023-07-18 18:56:14 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,48 @@
 #include "main.h"
-/**
- * print_times_table - Prints n is greater than 15 or less than 0
- * @n: The number to be printed
- * Return: Always 0
- */
 
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
 void print_times_table(int n)
 {
-int i, j, product;
+	int i, j, k;
 
-if (n >= 0 && n <= 15)
-{
-for (i = 0; i <= n; i++)
-{
-_putchar('0');
-for (j = 1; j <= n; j++)
-{
-_putchar(',');
-_putchar(' ');
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
 
-product = i * j;
-if (product <= 99)
-{
-_putchar(' ');
-}
-if (product <= 9)
-{
-_putchar(' ');
-}
-if (product >= 100)
-{
-_putchar((product / 100) + '0');
-_putchar((product / 100) % 10 + '0');
-}
-else if (product <= 99 && product >= 10)
-{
-_putchar((product / 10) + '0');
-}
-_putchar((product % 10) + '0');
-}
-_putchar('\n');
-}
-}
-}
